c4195137427eaa9832539f40eae4e15a68425709
caamomilaa <c.velazquezdamico@gmail.com>
2025-05-16 09:43:11 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import { GlobalStyles } from './styles/GlobalStyles';
+
 const App = () => {
-  return <h1>React</h1>;
+	return (
+		<>
+			<GlobalStyles />
+			<h1>React</h1>
+		</>
+	);
 };
 
 export default App;
